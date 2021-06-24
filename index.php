eaa7a36b0886d79eaed06f4ee955918eff1572d7
Erick <erick.andres2000@hotmail.com>
2021-06-24 16:10:48 -0500

    finalizacion

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php require 'vendor/autoload.php';
-    echo phpinfo();
-    echo "xd";
+    echo("Estos son tus datos: <br/>");
+    
+    echo "Tu(s) nombre(s): ". $_GET["nombre"]. "<br/>";
+    echo "Tu(s) apellido(s): ". $_GET["apellido"]. "<br/>";
     try {
         $client = new MongoDB\Client('');
-        $db = $client->databasename;
-        $collection = $db->collection_name;
+        $db = $client->desarrolloweb;
+        $collection = $db->formulario;
                 $document = $collection->find();
         foreach ($document as $item) {
-            echo $item['name'] . '</br>';
+            echo $item['nombre'] . '</br>';
         }
         echo "final";
             } catch (PDOException $e) {
